8abec332f1e0251ef748a1886a84285eee1d2e88
0utLunar <contatolucascir@gmail.com>
2026-09-11 17:43:03 -0300

    Finished functional coding and lambda exercises and examples

---
+++ 16 - Programação funcional e Expressões lambda/exercicio_01/src/application/Program.java
@@ -0,0 +1,58 @@
+package application;
+
+import entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter full file path: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            List<Employee> list = new ArrayList<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+
+            System.out.print("Enter salary: ");
+            double salary = sc.nextDouble();
+
+            System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary));
+
+            List<String> emails = list.stream()
+                    .filter(p -> p.getSalary() > 2000.00)
+                    .map(p -> p.getEmail()).sorted()
+                    .collect(Collectors.toList());
+
+            emails.forEach(System.out::println);
+
+            double sum  = list.stream()
+                    .filter(p -> p.getName().toUpperCase().charAt(0) == 'M')
+                    .map(p -> p.getSalary())
+                    .reduce(0.0, (x,y) -> x + y);
+
+            System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
+
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+
+}
